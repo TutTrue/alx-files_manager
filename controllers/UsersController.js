1c89f404d573b52e31ec24eef2b1c9e21dc5174d
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-03-13 07:12:03 +0200

    post user route

---
+++ controllers/UsersController.js
@@ -0,0 +1,34 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UserControler {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).send({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+
+    try {
+      if (await dbClient.client.db().collection('users').findOne({ email })) {
+        return res.status(400).send({ error: 'Already exist' });
+      }
+      const user = await dbClient.client
+        .db()
+        .collection('users')
+        .insertOne({ email, password: sha1(password) });
+      const insertedUser = user.ops[0];
+      return res
+        .status(201)
+        .json({ id: insertedUser._id, email: insertedUser.email });
+    } catch (error) {
+      return res.status(500).send({ error: 'Internal server error' });
+    }
+  }
+}
+
+export default UserControler;
